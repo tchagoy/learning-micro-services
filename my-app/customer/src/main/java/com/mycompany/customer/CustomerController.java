517a51fcb71a6d94127d2f94cf8c938591da9dbe
Tommy109 <55705991+Tommy109@users.noreply.github.com>
2022-09-01 16:30:31 -0600

    Added customer entity, customer controller, customer service, and customer registration request classes

---
+++ my-app/customer/src/main/java/com/mycompany/customer/CustomerController.java
@@ -0,0 +1,17 @@
+package com.mycompany.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public record CustomerController(CustomerService customerService) {
+
+    @PostMapping("api/vi/customers")
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
+        log.info("new customer registration {}", customerRegistrationRequest);
+        customerService.registerCustomer(customerRegistrationRequest);
+    }
+}
