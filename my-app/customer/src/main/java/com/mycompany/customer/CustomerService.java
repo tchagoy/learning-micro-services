c1aa1264ccafb595218e3ec6e16ca0d9ccb1ad1e
Tommy109 <arkaner89@gmail.com>
2022-09-02 11:46:17 -0600

    progress on connecting to db

--- my-app/customer/src/main/java/com/mycompany/customer/CustomerService.java
+++ my-app/customer/src/main/java/com/mycompany/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.mycompany.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -13,6 +13,7 @@ public record CustomerService() {
 
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: store customer in db
+
+        customerRepository.save(customer);
     }
 }
