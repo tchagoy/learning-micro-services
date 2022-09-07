84d9e7f95036c173f9354f14af89a5c33a36caae
Tommy109 <arkaner89@gmail.com>
2022-09-06 19:51:03 -0600

    added Customer entity

--- my-app/customer/src/main/java/com/mycompany/customer/Customer.java
+++ my-app/customer/src/main/java/com/mycompany/customer/Customer.java
@@ -1,13 +1,14 @@
 package com.mycompany.customer;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,4 +28,17 @@ public class Customer {
     private String firstName;
     private String lastName;
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Customer customer = (Customer) o;
+        return id != null && Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
